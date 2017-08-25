9b0d552f05fcd8293897755c3504321574639158
Daniel Koza <dakolech@gmail.com>
2017-08-25 15:05:10 +0200

    configure environment scripts

---
+++ copyEnvFiles.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cp src/environments/environment.prod.ts.example src/environments/environment.prod.ts
+cp src/environments/environment.stag.ts.example src/environments/environment.stag.ts
+cp src/environments/environment.dev.ts.example src/environments/environment.dev.ts
+cp src/environments/environment.ts.example src/environments/environment.ts
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import './App.css';
 import { CounterComponent } from './counter/components/counter.component';
+import { environment } from './environments';
+import { api } from './services/api.middleware';
 
+api.setBaseUrl(environment.URL);
 
 class App extends React.Component {
   render() {
